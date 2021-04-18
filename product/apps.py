3ce0b67847f09cb8b9ef58f99662c3d9b9989a2f
Allan Juliani <allan.s.juliani@gmail.com>
2021-04-18 12:17:44 -0300

    chore: update apps config

--- product/apps.py
+++ product/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class ProductConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
     name = 'product'
     verbose_name = _('Product')
